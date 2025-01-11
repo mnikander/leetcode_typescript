e5b74e96ab6f54cb1a979d28f0522bcd98cd28a5
Marco Nikander <m4n1k.developer@gmail.com>
2025-01-11 11:41:30 +0100

    unit test two-sum

--- test/0001-two-sum.test.ts
+++ test/0001-two-sum.test.ts
@@ -7,4 +7,12 @@ describe('twoSum', () => {
         expect(twoSum([0, 1], 1)).toStrictEqual([0, 1]);
     });
 
+    test('sequence', () => {
+        expect(twoSum([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41], 42)).toStrictEqual([7, 8]);
+    });
+
+    test('picking the same element twice (in this case #2) is not allowed', () => {
+        expect(twoSum([0, 1, 2, 3], 4)).toStrictEqual([1, 3]);
+    });
+
 });
